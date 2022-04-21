1df6ad7b03371d6de9f0275419519580e107ed57
GhabrielBender <ghabrielbmutti@gmail.com>
2022-04-21 17:41:51 -0300

    test: ensure RemoteAuthentication throws Unexpected error on default cases

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -3,7 +3,7 @@ import { RemoteAuthentication } from "./remote-authentication"
 import { mockAuthentication } from "@/domain/test/mock-authentication"
 import faker from 'faker'
 import { HttpStatusCode } from "@/data/protocols/http/http-response"
-import { InvalidCredentialsError } from "@/domain/errors"
+import { InvalidCredentialsError, UnexpectedError } from "@/domain/errors"
 
 type SutTypes = {
   sut: RemoteAuthentication
@@ -42,5 +42,23 @@ describe('RemoteAuthentication', () => {
     }
     const promise = sut.auth(mockAuthentication())
     await expect(promise).rejects.toThrow(new InvalidCredentialsError());
+  })
+
+  test("Should throw UnexpectedError if HttPostClient returns 400", async () => {
+    const {sut, httpPostClientSpy} = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest
+    }
+    const promise = sut.auth(mockAuthentication())
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  })
+
+  test("Should throw UnexpectedError if HttPostClient returns 404", async () => {
+    const {sut, httpPostClientSpy} = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    }
+    const promise = sut.auth(mockAuthentication())
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   })
 })
