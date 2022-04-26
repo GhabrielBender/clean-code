c6dae6773f64f2bff4d0095e42bfafa894518c5c
GhabrielBender <ghabrielbmutti@gmail.com>
2022-04-26 10:35:08 -0300

    add generic params to HttpClient

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,12 +1,13 @@
 import { HttpStatusCode } from "@/data/protocols/http/http-response"
 import { InvalidCredentialsError, UnexpectedError } from "@/domain/errors"
+import { AccountModel } from "@/domain/models"
 import { AuthenticationParams } from "@/domain/usecases/authentication"
 import { HttpPostClient } from "../../../data/protocols/http/http-post-client"
 
 export class RemoteAuthentication {
   constructor (
     private readonly url: string, 
-    private readonly httpPostClient: HttpPostClient
+    private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
   ) {}
 
   async auth (params: AuthenticationParams): Promise<void> {
