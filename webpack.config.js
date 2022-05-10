17a3cb3bf06c50634d08cdd85c6e1525f812b3ab
GhabrielBender <ghabrielbmutti@gmail.com>
2022-05-10 15:08:56 -0300

    chore: general configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,50 @@
-const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/main/index.tsx',
+  mode: "development",
+  entry: "./src/main/index.tsx",
   output: {
-    path: path.join(__dirname, 'public/js'),
-    publicPath: '/public/js',
-    path: path.join(__dirname, 'src'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [{ test: /\.tsx$/, use: 'babel-loader' }],
+    path: path.join(__dirname, "public/js"),
+    publicPath: "/public/js",
+    filename: "bundle.js",
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: [".ts", ".tsx", ".js"],
     alias: {
-      path: path.join(__dirname, 'src'),
+      path: path.join(__dirname, "src"),
     },
   },
+  module: {
+    rules: [
+      { test: /\.ts(x?)$/, use: "babel-loader", exclude: /node_modules/ },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
   devServer: {
-    static: './public',
+    static: "./public",
     // writeToDisk: true,
     historyApiFallback: true,
   },
   externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
+    react: "React",
+    "react-dom": "ReactDOM",
   },
   plugins: [new CleanWebpackPlugin()],
-}
+};
